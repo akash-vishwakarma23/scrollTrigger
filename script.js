5d02728d001c73f31761f8e239f127bf23cc0517
Akash Vishwakarma <75774551+akash-vishwakarma23@users.noreply.github.com>
2024-06-16 10:35:53 +0530

    added file

---
+++ script.js
@@ -0,0 +1,38 @@
+gsap.from("#page1 .circle",{
+    scale:0,
+    duration:2,
+    delay:0.5,
+    rotate:720,
+    
+
+})
+gsap.from("#page2 .circle",{
+    scale:0,
+    duration:2,
+    rotate:720,
+    scrollTrigger:{
+        trigger:"#page2 .circle",
+        scroller:"body",
+        markers:"true",
+        start:"top 60%",
+        end:"top 30%",
+        scrub:"5"
+
+    }
+
+})
+gsap.from("#page3 .circle",{
+    scale:0,
+    duration:2,
+    rotate:720,
+    scrollTrigger:{
+        trigger:"#page3 .circle",
+        scroller:"body",
+        markers:"true",
+        start:"top 70%",
+        end:"top 50%",
+        scrub:"5"
+
+    }
+
+})
